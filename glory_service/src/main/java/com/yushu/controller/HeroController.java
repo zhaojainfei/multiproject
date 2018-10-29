0a4b4202e63151aaeb7f0034efe4da2a99f37388
zhaojianfei <zhaojianfei@aoji.cn>
2018-10-29 18:02:11 +0800

    提交

--- glory_service/src/main/java/com/yushu/controller/HeroController.java
+++ glory_service/src/main/java/com/yushu/controller/HeroController.java
@@ -5,6 +5,7 @@ import com.yushu.service.HeroService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HeroController {
     private static final Logger logger = LoggerFactory.getLogger(HeroController.class);
 
+    @Value("${env}")
+    private String env;
+    @Value("${branch}")
+    private String branch;
+
     @Autowired
     HeroService heroService;
 
     @RequestMapping(value = "/get",method = RequestMethod.GET)
     public Object get(){
+        logger.info(env + " " + branch);
         logger.info("service调用方法 - /glory/hero/get");
         Hero hero = new Hero();
         return heroService.getList(hero);
